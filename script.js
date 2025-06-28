86b8654208a2d6b101bb7ce2b36abc98c228157d
Pedro Martins <pedromartiinss@icloud.com>
2025-06-28 17:34:21 -0300

    Atualizações.

--- script.js
+++ script.js
@@ -42,3 +42,12 @@ document.addEventListener("DOMContentLoaded", () => {
         }, 100);
     });
 });
+
+const acc = document.querySelectorAll(".accordion");
+    acc.forEach(btn => {
+        btn.addEventListener("click", function () {
+            this.classList.toggle("active");
+            const panel = this.nextElementSibling;
+            panel.style.display = panel.style.display === "block" ? "none" : "block";
+        });
+    });
